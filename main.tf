9a571ae158e3a5042a22c876fb481e4045d0f0d0
atudosan <atudosan@yahoo.com>
2021-06-28 23:12:11 -0700

    creating cluster

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-          name = "automation_terraform"
+          name = "new_workspace"
         }
       }
     }
