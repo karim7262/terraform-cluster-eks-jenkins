cc7ce9f711792431421b741ce98189e24db58f90
atudosan <atudosan@yahoo.com>
2021-07-06 00:07:13 -0700

    creating cluster

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
       required_version = ">= 0.14"
       required_providers {
         nirmata = {
-            source  = "nirmata/nirmata"
+            source  = "nirmata1/nirmata"
             # source  = "nirmata/nirmata"
             version = "1.0.0"
         }
