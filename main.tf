28c87215e99d49c31796d71940adf65c38ff2672
atudosan <atudosan@yahoo.com>
2021-07-07 01:45:41 -0700

    creating cluster

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
  # The configuration for the `remote` backend.
     terraform {
-      required_version = ">= 0.14"
-      required_providers {
-        nirmata = {
-            source  = "nirmata1/nirmata"
-            # source  = "nirmata/nirmata"
-            version = "1.0.0"
-        }
-      }
+      # required_version = ">= 0.14"
+      # required_providers {
+      #   nirmata = {
+      #       source  = "nirmata1/nirmata"
+      #       # source  = "nirmata/nirmata"
+      #       version = "1.0.0"
+      #   }
+      # }
       provider_installation{
         filesystem_mirror{
           path = "/home/runner/work/nirmata_terraform_cluster_eks/nirmata_terraform_cluster_eks/terraform-provider-nirmata"
