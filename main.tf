524384d88b0de692cf294f452b3636de890e10ce
atudosan <atudosan@yahoo.com>
2021-07-19 01:33:27 -0700

    old scripts

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@
 
         # The name of the Terraform Cloud workspace to store Terraform state files in
         workspaces {
-          name = "workspace11"
+          name = "workspace12"
         }
       }
     }
