eb04cecfef6142d7cbad9fc789e1be901aeb7981
atudosan <atudosan@yahoo.com>
2021-07-19 01:29:08 -0700

    old scripts

--- cluster_type_eks.tf
+++ cluster_type_eks.tf
@@ -15,12 +15,48 @@ provider "nirmata" {
 }
 resource "nirmata_cluster_type_eks" "eks-cluster-tf-test-automation" {
   name                      = "cluster-type-eks-test-${local.name-sufix}"
-  version                   = "1.18"
+#   version                   = "1.18"
+#   credentials               = "nirmata-aws-dev"
+#   region                    = "us-west-1"
+#   vpc_id                    = "vpc-0a02d3feafdd131e9"
+#   subnet_id                 = ["subnet-0369f4bd32e5db9d5", "subnet-0bea7061f60ae8a8d", "subnet-08f6b376fb8c0d61d"]
+#   security_groups           = ["sg-09d9fc7c49bed957f"]
+#   cluster_role_arn          = "arn:aws:iam::844333597536:role/eks-role"
+#   enable_private_endpoint   = true
+#   enable_identity_provider  = true
+#   auto_sync_namespaces       = false
+#   # enable_secrets_encryption = true
+#   # kms_key_arn = ""
+#   # log_types = ""
+#   # enable_fargate = true
+#   # pod_execution_role_arn = ""
+#   nodepools {
+#     name                = "default"
+#     instance_type       = "t2.small"
+#     disk_size           = 60
+#     ssh_key_name        = "devtest"
+#     security_groups     = ["sg-02fef934111f13a04"]
+#     iam_role            = "arn:aws:iam::844333597536:role/Node-IAM-Role"
+#   }
+
+# }
+# resource "nirmata_cluster" "eks-cluster-test-automation" {   
+#   name                 = "cluster-eks-test-${local.name-sufix}"
+#   cluster_type         = nirmata_cluster_type_eks.eks-cluster-tf-test-automation.name 
+#   nodepools {
+#   node_count                = 3
+#       enable_auto_scaling       = true
+#       min_count = 2
+#       max_count = 4
+#    }
+# }
+
+ version                   = "1.19"
   credentials               = "nirmata-aws-dev"
   region                    = "us-west-1"
-  vpc_id                    = "vpc-0a02d3feafdd131e9"
-  subnet_id                 = ["subnet-0369f4bd32e5db9d5", "subnet-0bea7061f60ae8a8d", "subnet-08f6b376fb8c0d61d"]
-  security_groups           = ["sg-09d9fc7c49bed957f"]
+  vpc_id                    = "vpc-04a11fc7db0fc1765"
+  subnet_id                 = ["subnet-0df29c38cf4a5", "subnet-011f4f72e6626fc69"]
+  security_groups           = ["sg-0f76dae17a0ef3211"]
   cluster_role_arn          = "arn:aws:iam::844333597536:role/eks-role"
   enable_private_endpoint   = true
   enable_identity_provider  = true
@@ -32,21 +68,24 @@ resource "nirmata_cluster_type_eks" "eks-cluster-tf-test-automation" {
   # pod_execution_role_arn = ""
   nodepools {
     name                = "default"
-    instance_type       = "t2.small"
+    instance_type       = "t3.medium"
     disk_size           = 60
-    ssh_key_name        = "devtest"
-    security_groups     = ["sg-02fef934111f13a04"]
+    ssh_key_name        = ""
+    security_groups     = ["sg-0acabab6d34120202"]
     iam_role            = "arn:aws:iam::844333597536:role/Node-IAM-Role"
   }
-
-}
-resource "nirmata_cluster" "eks-cluster-test-automation" {   
-  name                 = "cluster-eks-test-${local.name-sufix}"
-  cluster_type         = nirmata_cluster_type_eks.eks-cluster-tf-test-automation.name 
-  nodepools {
-  node_count                = 3
-      enable_auto_scaling       = true
-      min_count = 2
-      max_count = 4
-   }
+   vault_auth {
+    name             = "vault-auth"
+    path             = "nirmata/$(cluster.name)"
+    addon_name       = "vault-agent-injector"
+    credentials_name = "test-vault"
+    delete_auth_path     = "true"
+    roles {
+      name                 = "datadog-agent"
+      service_account_name = "datadog-agent"
+      namespace            = "datadog-agent"
+      policies             = "nirmata"
+      
+    }
+  }
 }
