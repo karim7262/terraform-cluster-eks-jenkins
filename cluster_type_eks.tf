2683d22276843e2c619aec38a51abf5a727ad2f6
atudosan <atudosan@yahoo.com>
2021-07-19 01:23:31 -0700

    without addons

--- cluster_type_eks.tf
+++ cluster_type_eks.tf
@@ -3,7 +3,7 @@ variable "nirmata_token"{
 } 
 
 locals {
-  name-sufix = "automation03" // !!! change also in terraform.yaml
+  name-sufix = "automation03" // !!! change also in terraform.yaml.
 }
 
 
