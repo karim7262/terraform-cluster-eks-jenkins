24f1e0aa3628f2f390d0a4d69cbb7e8d503c3160
Alexandru Tudosan <60152297+atudosan@users.noreply.github.com>
2021-07-29 12:32:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@
 
         # The name of the Terraform Cloud workspace to store Terraform state files in
         workspaces {
-          name = "workspace16"
+          name = "workspace17"
         }
         token = "nzRfitwaWGQFAA.atlasv1.1I6GmrUpJIIdIGr3208bruFloKj9RGW97CrRHTsdkfyOQ9cko09jRYjLB1eLOOW5JQ4"
       }
